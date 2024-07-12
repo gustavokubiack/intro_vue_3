11369d07396e36978c6f7a9eda99e05092e2cfa6
gustavokubiack <guukubiack@gmail.com>
2024-07-12 10:51:57 -0300

    feat: computed properties

--- main.js
+++ main.js
@@ -4,13 +4,12 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false },
+            ],
+            selectedVariant: 0,
         }
     },
     methods: {
@@ -19,6 +18,25 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title(){
+            return `${this.brand} ${this.product}`
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        isOnSale() {
+            if (this.variants[this.selectedVariant].onSale) {
+                return `${this.brand} ${this.product} is on sale!!`
+            }
         }
     }
 })
